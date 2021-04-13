5d3cc392bf6e7475b2c3b728f670a0f215db8b72
karthikeyan <karthikeyanr4java@gmail.com>
2021-04-13 14:00:47 +0530

    login with DB implementation

--- src/main/java/com/example/kklogin/config/service/MyUserDetailsService.java
+++ src/main/java/com/example/kklogin/config/service/MyUserDetailsService.java
@@ -2,19 +2,27 @@ package com.example.kklogin.config.service;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.example.kklogin.entity.Users;
+import com.example.kklogin.repo.UsersRepository;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService{
 
+	@Autowired
+	private UsersRepository usersRepository;
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return new User("admin","admin", new ArrayList<>());
+		Users  user = usersRepository.findByUsername(username).get();
+		System.out.println("kk>>> {}" + user.toString());
+		return new User(user.getUsername(),user.getPassword(), new ArrayList<>());
 	}
 
 }
